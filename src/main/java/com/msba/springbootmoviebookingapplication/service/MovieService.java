1cd71c6d0136f50fbd1106de9cec36b763fdbce3
Manan Mittal <mananmittal333@gmail.com>
2023-11-26 11:47:05 +0530

    first commit

---
+++ src/main/java/com/msba/springbootmoviebookingapplication/service/MovieService.java
@@ -0,0 +1,14 @@
+package com.msba.springbootmoviebookingapplication.service;
+
+import com.msba.springbootmoviebookingapplication.dto.MovieDto;
+
+import java.util.List;
+
+public interface MovieService {
+    MovieDto createMovie(MovieDto movieDto);
+    List<MovieDto> getAllMovies();
+    MovieDto getMovieById(long id);
+    MovieDto updateMovie(MovieDto movieDto, long id);
+    void deleteMovieById(long id);
+    MovieDto findByMovieName(String movieName);
+}
---
+++ src/test/java/com/msba/springbootmoviebookingapplication/SpringbootMovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msba.springbootmoviebookingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMovieBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
